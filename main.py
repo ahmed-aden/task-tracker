056a67c0a6b0aeec1de9b8521e63818cda600437
Ahmed <aade0017@student.monash.edu>
2025-12-04 08:31:13 +1100

    cli output

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+
+
+def main(): 
+    print("Please choose a command")
+    print("add 'task name' ")
+    print("delete 'id' ")
+    print("mark-in-progress 'id' ")
+    print("mark-done 'id'")
+    print("list")
+    print("list done")
+    print("list todo")
+    print("list in-progress")
+
+
+if __name__ == '__main__':
+    main()
